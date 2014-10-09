19579385ad0759a6f26cb14c28ab208e10b0fe42
Phil Gengler <phil@pgengler.net>
2014-10-09 18:22:02 -0400

    Run under Unicorn

---
+++ unicorn.rb
@@ -0,0 +1,9 @@
+root = "/srv/apps/track"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen 5003
+worker_processes 2
+timeout 30
