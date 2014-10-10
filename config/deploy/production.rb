d1b29f673676adbb0e8980e6f59890c569ca6304
Phil Gengler <phil@pgengler.net>
2014-10-09 20:25:46 -0400

    Set environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server 'hyperion.pgengler.net', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server 'hyperion.pgengler.net', user: 'deploy', roles: %w{web app db}
+set :rails_env, :production
