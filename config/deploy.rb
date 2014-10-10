a6ef3380e8e4b8bf1925c8fc4a10800538de7266
Phil Gengler <phil@pgengler.net>
2014-10-09 20:27:36 -0400

    Set :linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_to, '/srv/apps/track-assignment'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids}
 
 namespace :deploy do
 
